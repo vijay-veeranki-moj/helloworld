f4ce44322b821dfb192c9de97bfa67f445ae30d6
vijay-veeranki-moj <vijay.veeranki@digital.justice.gov.uk>
2019-03-03 01:30:55 +0000

    updated ecr

--- namespaces/live-1.cloud-platform.service.justice.gov.uk/vv-test/resources/ecr.tf
+++ namespaces/live-1.cloud-platform.service.justice.gov.uk/vv-test/resources/ecr.tf
@@ -6,21 +6,21 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-module "vv-ecr-cred-new" {
+module "vv-ecr-cred-news" {
   source    = "github.com/ministryofjustice/cloud-platform-terraform-ecr-credentials?ref=2.0"
-  repo_name = "vv-ecr-cred-new"
+  repo_name = "vv-ecr-cred-news"
   team_name = "test-webops-cred"
 }
 
-resource "kubernetes_secret" "vv-ecr-cred-new" {
+resource "kubernetes_secret" "vv-ecr-cred-news" {
   metadata {
-    name      = "ecr-repo-vv-myapp-new"
+    name      = "ecr-repo-vv-myapp-news"
     namespace = "vv-test"
   }
 
   data {
-    repo_url          = "${module.vv-ecr-cred-new.repo_url}"
-    access_key_id     = "${module.vv-ecr-cred-new.access_key_id}"
-    secret_access_key = "${module.vv-ecr-cred-new.secret_access_key}"
+    repo_url          = "${module.vv-ecr-cred-news.repo_url}"
+    access_key_id     = "${module.vv-ecr-cred-news.access_key_id}"
+    secret_access_key = "${module.vv-ecr-cred-news.secret_access_key}"
   }
 }
